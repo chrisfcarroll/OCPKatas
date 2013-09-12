de9ab3bb97cbb6c4a44fcdeb04d324b8c3cb2a24
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2013-09-12 13:34:51 +0100

    Undo checkin of partial solution to master

--- OCP Gilded Rose JS/OCP Kata Gilded Rose.js
+++ OCP Gilded Rose JS/OCP Kata Gilded Rose.js
@@ -1,4 +1,4 @@
-var gildedRoseStockList= function(){
+var GildedRose= function(){
 
   var me= {
     stockList : []
@@ -6,49 +6,3 @@ var gildedRoseStockList= function(){
 
   return me;
 }();
-
-var gildedRoseAge1Day= function(gr){
-  gr.age= function age(){
-    gr.stockList.forEach(function(item){
-      item.sellIn -=1;
-      item.quality -=1;
-    })
-  };
-  return gr;
-}
-
-//Although 'unmodified' this function becomes redundant once gildedRoseAgeAfterSellBy is defined ...
-var gildedRoseAgeNDays= function(gr){
-  gr.ageN= function ageN(days){
-    gr.stockList.forEach(function(item){
-      item.sellIn -=days;
-      item.quality -=days;
-    })
-  };
-  return gr;
-}
-
-var gildedRoseAgeAfterSellBy= function(gr){
-
-  gr.ageItem= function(item){
-    item.sellIn--;
-    var qualityLoss= item.sellIn<0 ? 2 : 1;
-    item.quality -= qualityLoss;
-  };
-
-  gr.ageN= function ageN(days){
-    for(var i=1; i<=days; i+=1){
-      gr.stockList.forEach( gr.ageItem );
-    }
-  };
-  return gr;
-}
-
-
-
-var GildedRoseFactory= function(){
-  return gildedRoseAgeAfterSellBy(
-         gildedRoseAge1Day(
-          Object.create(gildedRoseStockList)
-         ));
-};
