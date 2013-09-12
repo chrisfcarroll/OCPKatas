b1d90435f635fd1f4837b3c77f4c3053ca72838a
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2013-09-12 17:38:18 +0100

    The Gilded Mall JS - Attempt 1,  Gilded Dress & Gilded Tin Can

--- OCP Gilded Mall/Gilded Mall JS/OCP Kata Gilded Mall Specifications.js
+++ OCP Gilded Mall/Gilded Mall JS/OCP Kata Gilded Mall Specifications.js
@@ -1,10 +1,17 @@
-function gildedDressesTestCase1(){ return [ {name: "A dress", price: 10.00} ]; }
+function gildedDressesTestCase1(){
+  return [ {name: "A dress", daysInStock:0, price: 10.00, originalPrice:10.00} ];
+};
+
+function gildedTinCanTestCase1(){
+  return [ {name: "Tin of beans", daysInStock:0, price: 10.00, originalPrice:10.00, daysToSellBy:365} ];
+}
 
 describe("The Gilded Mall - Specifications.", function () {
 
   describe("All tenants", function () {
 
     theGildedDress.stockList = gildedDressesTestCase1();
+    theGildedTinCan.stockList= gildedTinCanTestCase1();
 
     for (var key in allShops) {
       var thisShop = allShops[key];
@@ -23,7 +30,68 @@ describe("The Gilded Mall - Specifications.", function () {
   });
 
   describe("The Gilded Dress", function(){
-    it("should",function(){ });
+
+    beforeEach(function(){
+      theGildedDress.stockList = gildedDressesTestCase1();
+    });
+
+    describe("For every week that passes",function(){
+
+      var originalPrices= theGildedDress.stockList.map(function(i){return _.clone(i);});
+
+      beforeEach(function(){
+        theGildedDress.age(7);
+      });
+
+      it("should reduce the sale price of each item by 10% of its original sale price after 1 week", function(){
+        for(var i=0; i< theGildedDress.stockList.length; i+=1){
+          expect(theGildedDress.stockList[i].price).toBe(originalPrices[i].price * 0.9);
+        }
+      });
+
+      it("should reduce the sale price of each item by 20% of its original sale price after 2 weeks", function(){
+        theGildedDress.age(7);
+        for(var i=0; i< theGildedDress.stockList.length; i+=1){
+          expect(theGildedDress.stockList[i].price).toBe(originalPrices[i].price * 0.8);
+        }
+      });
+
+      it("So long as the price hasn't fallen to 25% of original price, after which it stays flat",function(){
+        theGildedDress.age(7*8);
+        for(var i=0; i< theGildedDress.stockList.length; i+=1){
+          expect(theGildedDress.stockList[i].price).toBe(originalPrices[i].price * 0.25);
+        }
+      })
+
+    });
+
+  });
+
+  describe("The Gilded TinCan", function(){
+
+    beforeEach(function(){
+      theGildedTinCan.stockList= gildedTinCanTestCase1();
+    });
+
+    describe("When an item is more than 2 months from its sellby date",function(){
+
+      var originalPrices= theGildedTinCan.stockList.map(function(i){return _.clone(i);});
+
+      it("When an item is more than 2 months from its sell-by date it should sell for its original value", function(){
+        for(var i=0; i< theGildedTinCan.stockList.length; i+=1){
+          expect(theGildedTinCan.stockList[i].price).toBe(originalPrices[i].price);
+        }
+      });
+
+      it("When an item is less than 2 months from its sell-by date it should sell at half price", function(){
+        theGildedTinCan.age(360);
+        for(var i=0; i< theGildedTinCan.stockList.length; i+=1){
+          expect(theGildedTinCan.stockList[i].price).toBe(originalPrices[i].price * 0.5);
+        }
+      });
+
+    });
+
   });
 
 });
--- OCP Gilded Mall/Gilded Mall JS/OCP Kata Gilded Mall.js
+++ OCP Gilded Mall/Gilded Mall JS/OCP Kata Gilded Mall.js
@@ -1,7 +1,8 @@
 var gildedStockList= function(){
 
   var me= {
-    stockList : []
+    stockList : [],
+    age : function(){}
   };
 
   return me;
@@ -11,8 +12,40 @@ var GildedShopFactory= function(shopName){
   return  Object.create(gildedStockList,{shopName:{value:shopName}});
 };
 
-var theGildedDress = GildedShopFactory("Gilded Dress");
+var GildedDressFactory= function(){
+  var me= GildedShopFactory("Gilded Dress");
+  me.age= function(days){
+    me.stockList.forEach(function(item){
+      var weeksPassed= Math.round( (item.daysInStock+days) /7) ;
+      if(weeksPassed<=7){
+        item.price = item.originalPrice * (1 - 0.10 * weeksPassed);
+      }else{
+        item.price = item.originalPrice * 0.25;
+      }
+      item.daysInStock+=days;
+    });
+  }
+  return me;
+};
+
+var GildedTinFactory= function(){
+  var me= GildedShopFactory("The Tin Can");
+  me.age=function(days){
+    me.stockList.forEach(function(item){
+      if(item.daysToSellBy>61){
+        item.price= item.originalPrice * 0.5;
+      }
+      item.daysInStock +=days;
+      item.daysToSellBy -=days;
+    });
+  };
+  return me;
+}
+
+var theGildedDress = GildedDressFactory();
+var theGildedTinCan = GildedTinFactory();
 
 var allShops= {
-  gildedDress : theGildedDress
+  gildedDress  : theGildedDress,
+  gildedTinCan : theGildedTinCan
 };
